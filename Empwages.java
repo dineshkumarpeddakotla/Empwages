f32044fc97380082dde5d7635d6ce4f7cbc80fcf
DINESH KUMAR <dineshkumar.icon@gmail.com>
2021-03-25 05:49:51 +0530

    Added Dailywage

--- Empwages.java
+++ Empwages.java
@@ -2,6 +2,7 @@ package empwages;
 
 class Empwages{
 public static final int Present = 1;
+public static final int Wage_perhr=20;
 public static void main(String[] args){
 System.out.println("Welcome to Employee");
  Empwage emp = new Empwage();
@@ -10,8 +11,11 @@ System.out.println("Welcome to Employee");
 public static class Empwage {
 public void Emp_wage() {
 int empcheck = (int) (Math.random() * 2);
+int emphrs=8;
 if(empcheck==Present){
 System.out.println("Employee is Present");
+int Daily_wage=Wage_perhr*emphrs;
+System.out.println("Daily Emp Wage =" +Daily_wage);
 }else{
 System.out.println("Employee is Absent");
 }
